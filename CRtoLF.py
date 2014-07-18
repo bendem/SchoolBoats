e2d1279269340c8f6dd198f1d28d7a6d4a0c73d8
bendem <online@bendem.be>
2014-07-18 22:14:47 +0200

    Update py script

--- CRtoLF.py
+++ CRtoLF.py
@@ -1,13 +1,5 @@
 import os
 
-def file_get_contents(filename):
-    with open(filename, 'r') as f:
-        return f.read()
-
-def file_put_contents(filename, content):
-    with open(filename, 'w') as f:
-        f.write(content)
-
 def get_files(folder, found, include):
     for f in os.listdir(folder):
         if os.path.isdir(os.path.join(folder, f)):
@@ -16,5 +8,8 @@ def get_files(folder, found, include):
             found.append(os.path.join(folder, f))
     return found
 
-for f in get_files('.', [], ['.hpp', '.cpp']):
-    file_put_contents(f, file_get_contents(f).replace('\r', '\n'))
+for filename in get_files('.', [], ['.hpp', '.cpp']):
+    with open(filename, 'r') as f:
+        c = f.read()
+    with open(filename, 'w') as f:
+        f.write(c.replace('\r', '\n'))
