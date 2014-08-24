125c3dd7b879a812e20c2d30c153928cf38614e9
bendem <online@bendem.be>
2014-08-24 15:32:08 +0200

    Prepare Test7

---
+++ headers/boats/serialization/RegattaFile.hpp
@@ -0,0 +1,23 @@
+#ifndef REGATTAFILE_HPP
+#define REGATTAFILE_HPP
+
+#include <iostream>
+#include <string>
+#include "boats/Regatta.hpp"
+
+using namespace std;
+
+class RegattaFile {
+	private:
+		string filename;
+
+	public:
+		RegattaFile(const string&);
+
+		void list() const;
+		int search(Regatta&, string) const; // TODO Throw an exception instead of returning 0 (such teacher D:)
+		void save(const Regatta&) const;
+
+};
+
+#endif
---
+++ src/boats/serialization/RegattaFile.cpp
@@ -0,0 +1,5 @@
+#include "boats/serialization/RegattaFile.hpp"
+
+RegattaFile::RegattaFile(const string& filename) {
+	this->filename = filename;
+}
